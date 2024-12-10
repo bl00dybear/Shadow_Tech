f5649900bc7ffe27537a20027254bfad4c9cfb1c
bl00dybear <sincarisebastian@gmail.com>
2024-12-10 14:49:39 +0200

    qwerty

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,9 +1,17 @@
-﻿namespace Shadow_Tech.Models
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Shadow_Tech.Models
 {
     public class Category
     {
         public int Id { get; set; }
         public string? Name { get; set; }
+        [Required(ErrorMessage = "Category description is required")]
+        public string? Description { get; set; }
         public ICollection<Product>? Products { get; set; }
+        
     }
 }
