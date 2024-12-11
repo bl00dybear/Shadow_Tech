86304173d86517836ef1991c96b10400a3a6f8ee
ghrghbogdan <150747865+ghrghbogdan@users.noreply.github.com>
2024-12-11 23:58:45 +0200

    mhjgc

--- Models/Review.cs
+++ Models/Review.cs
@@ -1,10 +1,17 @@
-﻿namespace Shadow_Tech.Models
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Shadow_Tech.Models
 {
     public class Review
     {
         public int Id { get; set; }
         public int ProductId { get; set; }
         public Product? Product { get; set; }
+        [Required(ErrorMessage = "Ratingul este obligatoriu")]
         public int Rating { get; set; }
         public string? Comment { get; set; }
         public string? UserId { get; set; }
