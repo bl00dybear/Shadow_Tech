b3a0eb027151beb794dd6c901498f3d42e78f7ba
bl00dybear <sincarisebastian@gmail.com>
2025-01-11 13:50:00 +0200

    altceva

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shadow_Tech.Data;
+using Shadow_Tech.Models;
+
+namespace Shadow_Tech.Controllers
+{
+    
+    public class OrdersController : Controller
+    {
+        private readonly ApplicationDbContext db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public OrdersController(
+        ApplicationDbContext context,
+        UserManager<ApplicationUser> userManager,
+        RoleManager<IdentityRole> roleManager
+        )
+        {
+            db = context;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+        public IActionResult Index()
+        {
+            var cartItems = db.Carts.Where(i => i.UserId == _userManager.GetUserId(User)).ToList();
+            return View(cartItems);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Shadow_Tech
+![Shadow_Tech](../images/2025-01-11 13-34-16.gif)
  
